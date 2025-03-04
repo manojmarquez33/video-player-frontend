8a5b67cfde07ebe160cac3cdfa21e538ba4d07c0
manojmarquez33 <manojmarquez33@gmail.com>
2025-03-04 21:54:09 +0530

    tabs updated for video and playlist

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,79 +3,57 @@ import $ from 'jquery';
 
 export default Ember.Controller.extend({
   searchQuery: '',
-  isVideoTab: true,
-  isPlaylistTab: false,
 
   actions: {
-    showVideos() {
-      this.set('isVideoTab', true);
-      this.set('isPlaylistTab', false);
-      this.send('fetchAllVideos');
-    },
-
-    showPlaylists() {
-      this.set('isVideoTab', false);
-      this.set('isPlaylistTab', true);
-      this.send('fetchAllPlaylists');
-    },
-
-    searchVideos() {
+    searchContent() {
       let query = this.get('searchQuery').trim();
       if (!query) {
-        this.get('isVideoTab') ? this.send('fetchAllVideos') : this.send('fetchAllPlaylists');
+        this.send('fetchAllContent');
         return;
       }
 
-      let apiUrl = this.get('isVideoTab')
-        ? `http://localhost:8080/VideoPlayer_war_exploded/VideoServlet?search=${encodeURIComponent(query)}`
-        : `http://localhost:8080/VideoPlayer_war_exploded/PlaylistServlet?search=${encodeURIComponent(query)}`;
-
-      $.ajax({
-        url: apiUrl,
-        type: 'GET',
-        dataType: 'json',
-        success: (data) => {
-          if (this.get('isVideoTab')) {
-            this.set('model.videos', data);
-          } else {
-            this.set('model.playlists', data);
-          }
-        },
-        error: (err) => {
-          console.error("Search request failed", err);
-          this.get('isVideoTab') ? this.set('model.videos', []) : this.set('model.playlists', []);
-        }
+      $.when(
+        $.ajax({
+          url: `http://localhost:8080/VideoPlayer_war_exploded/VideoServlet?search=${encodeURIComponent(query)}`,
+          type: 'GET',
+          dataType: 'json'
+        }),
+        $.ajax({
+          url: `http://localhost:8080/VideoPlayer_war_exploded/PlaylistServlet?search=${encodeURIComponent(query)}`,
+          type: 'GET',
+          dataType: 'json'
+        })
+      ).done((videoData, playlistData) => {
+        this.set('model.videos', videoData[0]);
+        this.set('model.playlists', playlistData[0]);
+      }).fail((err) => {
+        console.error("Search request failed", err);
+        this.set('model.videos', []);
+        this.set('model.playlists', []);
       });
     },
 
-    fetchAllVideos() {
-      $.ajax({
-        url: "http://localhost:8080/VideoPlayer_war_exploded/VideoServlet",
-        type: "GET",
-        dataType: "json",
-        success: (data) => {
-          this.set("model.videos", data);
-        },
-        error: (err) => {
-          console.error("Failed to fetch videos", err);
-          this.set("model.videos", []);
-        }
-      });
-    },
-
-    fetchAllPlaylists() {
-      $.ajax({
-        url: "http://localhost:8080/VideoPlayer_war_exploded/PlaylistServlet",
-        type: "GET",
-        dataType: "json",
-        success: (data) => {
-          this.set("model.playlists", data);
-        },
-        error: (err) => {
-          console.error("Failed to fetch playlists", err);
-          this.set("model.playlists", []);
-        }
+    fetchAllContent() {
+      $.when(
+        $.ajax({
+          url: "http://localhost:8080/VideoPlayer_war_exploded/VideoServlet",
+          type: "GET",
+          dataType: "json"
+        }),
+        $.ajax({
+          url: "http://localhost:8080/VideoPlayer_war_exploded/PlaylistServlet",
+          type: "GET",
+          dataType: "json"
+        })
+      ).done((videoData, playlistData) => {
+        this.set('model.videos', videoData[0]);
+        this.set('model.playlists', playlistData[0]);
+      }).fail((err) => {
+        console.error("Failed to fetch content", err);
+        this.set('model.videos', []);
+        this.set('model.playlists', []);
       });
     }
   }
 });
+
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
-      videos: Ember.$.getJSON('http://localhost:8080/VideoPlayer_war_exploded/VideoServlet'),
-      playlists: Ember.$.getJSON('http://localhost:8080/VideoPlayer_war_exploded/PlaylistServlet')
+      playlists: Ember.$.getJSON('http://localhost:8080/VideoPlayer_war_exploded/PlaylistServlet'),
+      videos: Ember.$.getJSON('http://localhost:8080/VideoPlayer_war_exploded/VideoServlet')
+
     });
   }
 });
